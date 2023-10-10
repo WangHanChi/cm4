c6e4a48ed0684ce74d607916cd420216cfa7f7ef
WangHanChi <hank1122334455667788990@gmail.com>
2023-10-10 17:00:06 +0800

    Import mini shell for cm4

---
+++ shell/command.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h>
+
+void input_buffer_overflow_message(char str[]);
+void command_help();
+void command_hello();
+void command_not_found(char *);
+void command_reboot();
+void command_sudo_su();
+void command_lscpu();
+void command_exit();
---
+++ shell/shell.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define MAX_BUFFER_LENGTH   80
+
+void shell_start();
+void parse(char str[]);
